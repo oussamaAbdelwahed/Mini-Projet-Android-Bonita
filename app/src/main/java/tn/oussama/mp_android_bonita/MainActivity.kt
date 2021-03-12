56575ea80485f67b6e329b8c5426b326e04923cc
oussamaAbdelwahed <ousabdelwahed@gmail.com>
2021-03-12 22:08:57 +0100

    Creating Project Skeleton

---
+++ app/src/main/java/tn/oussama/mp_android_bonita/MainActivity.kt
@@ -0,0 +1,11 @@
+package tn.oussama.mp_android_bonita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
